f8614b42e5bbc4a1779cc6ec6e539a15915ad961
Sedly <sedly.keith@student.moringaschool.com>
2024-05-29 14:21:08 +0300

    python-p3-fileio-lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode = 'w') as f:
+        f.write(file_content)
+    
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode = 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', mode = 'r') as f:
+        return f.read()
+
+# Example usage
+write_file('logfile', 'Log 1: 5 bananas added')
+
+append_file('logfile', 'Log 2: 3 bananas added')
+
+read_file('logfile')
+
+print(read_file('logfile'))
+    
